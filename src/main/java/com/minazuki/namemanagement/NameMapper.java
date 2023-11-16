0d71547077a6540258acb2158d3e6c4d5042b781
minazuki <aratanotennis@gmail.com>
2023-11-17 05:49:19 +0900

    課題提出９

--- src/main/java/com/minazuki/namemanagement/NameMapper.java
+++ src/main/java/com/minazuki/namemanagement/NameMapper.java
@@ -1,11 +1,10 @@
-// NameMapper.java ファイル
-
 package com.minazuki.namemanagement;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
+
 
 @Mapper
 public interface NameMapper {
